47d49c77fbfd2714b703e125ad1fa9f3bffffe4f
Alexander Schmutz <alexander.schmutz@maibornwolff.de>
2021-08-15 16:00:26 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alexander Schmutz on 15.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MSALSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSALSwiftUIApp.swift
+//  Shared
+//
+//  Created by Alexander Schmutz on 15.08.21.
+//
+
+import SwiftUI
+
+@main
+struct MSALSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
